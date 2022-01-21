cafdb505c38af4e0aa33720b538049a21ae9cf7f
Recep Sivri <recep.sivri@mahrek.com.tr>
2022-01-21 12:39:43 +0300

    version updated

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/rsivri-data-grid'),
+      dir: require('path').join(__dirname, './coverage/deneme'),
       subdir: '.',
       reporters: [
         { type: 'html' },
