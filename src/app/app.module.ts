4ec2e971bbebaddc388a3003b736945719a1d812
Recep Sivri <recep.sivri@mahrek.com.tr>
2022-05-11 15:27:41 +0300

    project infrastructure refactored

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {RsivriGridComponent} from "./rsivri-grid/rsivri-grid.component";
+import {RsivriGridHeaderComponent} from "./rsivri-grid/grid-header/rsivri-grid-header.component";
+import {RsivriGridBodyComponent} from "./rsivri-grid/grid-body/rsivri-grid-body.component";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    RsivriGridComponent
+    RsivriGridComponent,
+    RsivriGridHeaderComponent,
+    RsivriGridBodyComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
